f5ad65f715471557748cf4a56f2da91e402cdbfd
Teobaldo Moura <teobaldo.bh@gmail.com>
2018-04-05 21:47:33 -0300

    Criação do Hello Word controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers {
+    
+    //[Route("api/[controller]")]
+    public class HelloWorldController: Controller {
+
+
+        // 
+        // GET: /HelloWorld/
+        
+        //[HttpGet]
+        public string Index() {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        
+        //[HttpGet("welcome/{id?}")]
+        public string Welcome(string name, int ID = 1) {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
